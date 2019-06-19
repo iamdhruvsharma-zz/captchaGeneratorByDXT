e1d2dfc1267b65f1ea707cc7005fa22b3a26482c
Dhruv Sharma <42893986+iamdhruvsharma@users.noreply.github.com>
2019-06-19 22:07:09 +0530

    Updated Source File (Refactored)

--- captchaGenerator2.c
+++ captchaGenerator2.c
@@ -7,15 +7,15 @@ int main()
   char globalCharacters[ ] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '@', '$', '#', '&'};
   int i;
   int size;
-  printf("Please enter the size of Captcha you want to generate: ");
+  printf("Please enter the size of Captcha you want to generate: "); //user-defined custom size
   scanf("%d", &size);
   fflush(stdin);
   char captcha[size];
   char userInput[size];
-  srand(time(0));
+  srand(time(0));            //time(0) is used to make sure, code generated new random every time
   for(i=0; i<size; i++)
   {
-    captcha[i] = globalCharacters[rand()%62];
+    captcha[i] = globalCharacters[rand()%62]; 
   }
   printf("Captcha: ");
   for(i=0; i<size; i++)
@@ -30,7 +30,7 @@ int main()
   int check=0;
   for(i=0; i<size; i++)
   {
-    if(userInput[i] != captcha[i])
+    if(userInput[i] != captcha[i])  //Final Verfication
     {
       check=1;
       break;
